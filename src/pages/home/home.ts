2b22f5ffa12aabf9857704c889db58fe6d2c0685
iddiaz <diazdiazivan@gmail.com>
2017-07-23 00:52:40 +0200

    Configuración basica de págians y servicios de aplicación

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,13 +1,17 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
+import { ProductosProvider } from './../../providers/productos/productos';
+
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, private productosProvider: ProductosProvider) {
+
+   
 
   }
 
---
+++ src/providers/productos/productos.ts
@@ -0,0 +1,37 @@
+import { Injectable } from '@angular/core';
+import { Http } from '@angular/http';
+import 'rxjs/add/operator/map';
+
+import { URL_SERVICIOS } from './../../config/url.servicios';
+
+
+@Injectable()
+export class ProductosProvider {
+
+  pagina: number = 0;
+  productos: any[] = []; 
+
+  constructor(public http: Http) {
+    // console.log('Hello ProductosProvider Provider');
+    this.cargarTodos();
+  }
+
+  cargarTodos(){
+    let url = `${URL_SERVICIOS}/productos/todos/${this.pagina}`;
+
+    this.http.get( url )
+      .map( res => res.json() )
+      .subscribe( data => {
+        console.log(data);
+        if( data.error ){
+          //aqui hay un problema
+
+        } else {
+          this.productos.push(...data.productos);
+          this.pagina += 1;
+        }
+      })
+
+  }
+
+}
